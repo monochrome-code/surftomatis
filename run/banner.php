7e6e4ebb8f7e634137eec3eea19ed4d3ef2bbaac
monochrome-code <76603785+monochrome-code@users.noreply.github.com>
2020-12-26 05:06:38 -0800

    Update banner.php

--- run/banner.php
+++ run/banner.php
@@ -11,7 +11,7 @@ echo "\033[1;93m================================================================
 echo "\033[1;33m| \033[1;31m20 User-Agent mobile browser that changes automatically every time it makes a get request \033[1;33m|\n";
 echo "\033[1;33m| \033[1;31mWait 10 seconds to make the next request                                                  \033[1;33m|\n";
 echo "\033[1;33m| \033[1;31mIncrease automatic traffic to specific links                                              \033[1;33m|\n";
-echo "\033[1;33m|\033[41m\033[1;37m                              Created by monochrome-code                                   \033[1;93m|\n";
+echo "\033[1;33m|\033[41m\033[1;37m                              Created by monochrome-code                                   \033[0;39m\033[1;93m|\n";
 echo "\033[0;39m\033[1;93m=========================================ENJOY===============================================\n";
 
 ?>
