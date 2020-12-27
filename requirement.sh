4bbfacd99b5e96f8b502db797d0948df80afa570
monochrome-code <76603785+monochrome-code@users.noreply.github.com>
2020-12-27 06:32:29 -0500

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -2,21 +2,26 @@
 
 sudo chmod +x run.sh
 clear
-echo "\n\033[0;93m==UPDATE REPO==\033[m"
-sudo apt update
-echo "\n"
+echo "===UPDATE REPO==="
+sudo apt update -y
+echo "
+"
 
-echo "\033[0;93m==INSTALL PHP==\033[0m"
-echo sudo apt install php
-echo "\n"
+echo "===INSTALL PHP==="
+echo sudo apt install php -y
+echo "
+"
 
-echo "\033[0;93m==INSTALL PHP-CLI==\033[0m"
-sudo apt install php-cli
-echo "\n"
+echo "===INSTALL PHP-CLI==="
+sudo apt install php-cli -y
+echo "
+"
 
-echo "\033[0;93m==INSTALL PHP-CURL==\033[0m"
-sudo apt install php-curl
-echo "\n"
-echo "\033[0;93m============================"
+echo "===INSTALL PHP-CURL===m"
+sudo apt install php-curl -y
+echo "
+"
+
+echo "============================"
 echo "          SELESAI           "
 echo "============================"
